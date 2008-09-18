ffc2529f574c145a90b6e2e831200a91431762d4
Vincent Batts <vbatts@hashbangbash.com>
2008-09-18 13:45:10 +0000

    making more appropriate ATI adjustments

--- projects/boot-xorg-set/src/boot-xorg-set.sh
+++ projects/boot-xorg-set/src/boot-xorg-set.sh
@@ -8,14 +8,16 @@ for opt in $(cat /proc/cmdline ); do
   fi 
 done
 echo "setting display per $DISPLAY"
-if [ ${DISPLAY} = "DEFAULT" ] ; then
-  cp -v /etc/X11/xorg.conf.WORKS_fglrx /etc/X11/xorg.conf
+if [ ${DISPLAY} = "single" ] ; then
+  aticonfig --dtop=single
+  #cp -v /etc/X11/xorg.conf.WORKS_fglrx /etc/X11/xorg.conf
 elif [ ${DISPLAY} = "fglrx" ] ; then
   cp -v /etc/X11/xorg.conf.WORKS_fglrx /etc/X11/xorg.conf
 elif [ ${DISPLAY} = "radeon" ] ; then
   cp -v /etc/X11/xorg.conf.WORKS_radeon /etc/X11/xorg.conf
-elif [ ${DISPLAY} = "d1280" ] ; then
-  cp -v /etc/X11/xorg.conf.dualhead /etc/X11/xorg.conf
+elif [ ${DISPLAY} = "dual" ] ; then
+  aticonfig --dtop=horizontal,reverse --screen-layout=left
+  #cp -v /etc/X11/xorg.conf.dualhead /etc/X11/xorg.conf
 else
   cp -v /etc/X11/xorg.conf.WORKS_fglrx /etc/X11/xorg.conf
 fi
