a0286ac4e968c43d4c618bf7060a3fcf0ff58103
Vincent Batts <vbatts@hashbangbash.com>
2008-12-09 03:21:42 +0000

    more tweaks

--- projects/hudson/hudson.sh
+++ projects/hudson/hudson.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # $Id$
 
+#set -x
+
 JAVA=$(which java)
 HUDSON="/usr/lib/hudson/hudson.war"
 HUDSON_HTTP_PORT="8080"
 HUDSON_LOG_FILE="/var/log/hudson.log"
-HUDSON_HOME="/var/lib/hudson/"
+HUDSON_WEBAPPSDIR="/var/lib/hudson/apps/"
+HUDSON_WEBROOT="/var/lib/hudson/webroot/"
 
 
 if [ -f /etc/hudson.conf ] ; then
@@ -13,39 +16,43 @@ if [ -f /etc/hudson.conf ] ; then
 fi
 
 if [ "$HUDSON_HTTP_PORT" != "" ] ; then
-  HUDSON_HTTP_PORT_ARG="--httpPort $HUDSON_HTTP_PORT"
+  HUDSON_HTTP_PORT_ARG="--httpPort=$HUDSON_HTTP_PORT"
 fi
 
 if [ "$HUDSON_HTTP_LISTENING_ADDRESS" != "" ] ; then
-  HUDSON_HTTP_LISTENING_ADDRESS_ARG="--httpListenAddress $HUDSON_HTTP_LISTENING_ADDRESS"
+  HUDSON_HTTP_LISTENING_ADDRESS_ARG="--httpListenAddress=$HUDSON_HTTP_LISTENING_ADDRESS"
 fi
 
 if [ "$HUDSON_HTTPS_PORT" != "" ] ; then
-  HUDSON_HTTPS_PORT_ARG="--httpsPort $HUDSON_HTTPS_PORT"
+  HUDSON_HTTPS_PORT_ARG="--httpsPort=$HUDSON_HTTPS_PORT"
 fi
 
 if [ "$HUDSON_HTTPS_LISTENING_ADDRESS" != "" ] ; then
-  HUDSON_HTTPS_LISTENING_ADDRESS_ARG="--httpsListenAddress $HUDSON_HTTPS_LISTENING_ADDRESS"
+  HUDSON_HTTPS_LISTENING_ADDRESS_ARG="--httpsListenAddress=$HUDSON_HTTPS_LISTENING_ADDRESS"
 fi
 
 if [ "$HUDSON_HTTPS_KEYSTORE" != "" ] ; then
-  HUDSON_HTTPS_KEYSTORE_ARG="--httpsKeyStore $HUDSON_HTTPS_KEYSTORE"
+  HUDSON_HTTPS_KEYSTORE_ARG="--httpsKeyStore=$HUDSON_HTTPS_KEYSTORE"
 fi
 
 if [ "$HUDSON_HTTPS_KEYSTORE_PASSWORD" != "" ] ; then
-  HUDSON_HTTPS_KEYSTORE_PASSWORD_ARG="--httpsKeyStorePassword $HUDSON_HTTPS_KEYSTORE_PASSWORD"
+  HUDSON_HTTPS_KEYSTORE_PASSWORD_ARG="--httpsKeyStorePassword=$HUDSON_HTTPS_KEYSTORE_PASSWORD"
 fi
 
 if [ "$HUDSON_HTTPS_KEY_MANAGER" != "" ] ; then
-  HUDSON_HTTPS_KEY_MANAGER_ARG="--httpsKeyManagerType $HUDSON_HTTPS_KEY_MANAGER"
+  HUDSON_HTTPS_KEY_MANAGER_ARG="--httpsKeyManagerType=$HUDSON_HTTPS_KEY_MANAGER"
 fi
 
 if [ "$HUDSON_LOG_FILE" != "" ] ; then
-  HUDSON_LOG_FILE_ARG="--logfile $HUDSON_LOG_FILE"
+  HUDSON_LOG_FILE_ARG="--logfile=$HUDSON_LOG_FILE"
 fi
 
-if [ "$HUDSON_HOME" != "" ] ; then
-  HUDSON_HOME_ARG="--webroot $HUDSON_HOME"
+if [ "$HUDSON_WEBAPPSDIR" != "" ] ; then
+  HUDSON_WEBAPPSDIR_ARG="--webapssDir=$HUDSON_WEBAPPSDIR"
+fi
+
+if [ "$HUDSON_WEBROOT" != "" ] ; then
+  HUDSON_WEBROOT_ARG="--webroot=$HUDSON_WEBROOT"
 fi
 
 exec $JAVA -jar $HUDSON \
@@ -57,4 +64,5 @@ exec $JAVA -jar $HUDSON \
   $HUDSON_HTTPS_KEYSTORE_PASSWORD_ARG \
   $HUDSON_HTTPS_KEY_MANAGER_ARG \
   $HUDSON_LOG_FILE_ARG \
-  $HUDSON_HOME_ARG
+  $HUDSON_WEBAPPSDIR_ARG \
+  $HUDSON_WEBROOT_ARG
