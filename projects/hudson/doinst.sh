e778658c8c2d02985adba19e425989eb7118370a
Vincent Batts <vbatts@hashbangbash.com>
2010-08-25 13:42:57 -0400

    hudson: more cleanup

--- projects/hudson/doinst.sh
+++ projects/hudson/doinst.sh
@@ -14,12 +14,18 @@ config etc/hudson.conf.new
 config etc/rc.d/rc.hudson.new
 
 if ! grep -qw hudson /etc/group ; then
+	echo -ne "adding hudson group : "
+	echo "groupadd -g 300 -r hudson"
 	groupadd -g 300 -r hudson
 fi
 
 if ! grep -qw hudson /etc/passwd ; then
+	echo -ne "adding hudson user : "
+	echo "useradd -k /dev/null -g 300 -M -r -s /bin/bash -d /var/lib/hudson -g hudson hudson"
 	useradd -k /dev/null -g 300 -M -r -s /bin/bash -d /var/lib/hudson -g hudson hudson
 fi
 
+echo -ne "changing ownership of hudson's home : "
+echo "chown -R hudson.hudson /var/lib/hudson"
 chown -R hudson.hudson /var/lib/hudson
 
