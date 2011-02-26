70ce0488dec34775c2a227d45182331dfb41720c
Vincent Batts <vbatts@hashbangbash.com>
2011-02-26 16:17:15 -0500

    final touches on rubinius

---
+++ TESTING/rubinius/doinst.sh
@@ -0,0 +1,8 @@
+
+n="rubinius: "
+
+echo "$n to use rubinius, you will need to do"
+echo "$n #> chmod +/etc/profile.d/rubinius-\$VERSION.*sh"
+echo "$n caution: this will override the system ruby"
+
+
