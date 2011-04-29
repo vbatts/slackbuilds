c38b183c87c9e6e852888aa8deea53d96d8e7bd9
Vincent Batts <vbatts@hashbangbash.com>
2011-04-29 16:36:00 -0400

    adding lists to reconcile, and produce a TODOs list

--- published.sh
+++ published.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-SBO_DIR=${SBO_DIR:-/var/lib/sbopkg/SBo/}
+VERSION=${VERSION:-13.37}
+SBO_DIR=${SBO_DIR:-/var/lib/sbopkg/SBo/$VERSION/}
 SEARCH_STRING=${1:-vbatts}
 LIST=$(find $SBO_DIR -type f -name '*.info' | xargs grep $SEARCH_STRING | cut -d : -f 1 | sort -u )
 
